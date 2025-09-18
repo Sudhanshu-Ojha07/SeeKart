e4eb00852c550fe0d9cd50172b64cd51d6fae40b
Sudhanshu-Ojha07 <sudhanshuojha01643@gmail.com>
2025-09-18 14:20:29 +0530

    Changed Allowed Host for Render

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -27,8 +27,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-0#rptt8oc37d$ccax5yy0d4&jrekifowaum_suze2*&#7fbs@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+DEBUG = True
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')  # your Render host from env
+
 
 
 # Application definition
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,6 +60,8 @@ MIDDLEWARE = [
     # 'accounts.middleware.restrict_admin.SeparateSessionMiddleware',
     # 'accounts.middleware.restrict_admin.AdminSiteSessionMiddleware', 
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'ecom.urls'
 
